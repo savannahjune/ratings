b36ba034e7ce4eb9a8f32acc76bdda712122cfdc
Hackbright Student <info@hackbrightacademy.com>
2014-10-24 12:08:19 -0700

    finished part 1

--- seed.py
+++ seed.py
@@ -21,7 +21,7 @@ def load_movies(session):
         for row in reader:
             if row[2] == "":
                 row[2] = "01-Jan-1970"
-            movie = Movie(id = row[0], name = row[1].encode('utf-8'), release_date = datetime.strptime(row[2].strip(),"%d-%b-%Y"), imdb_url = row[3])
+            movie = Movie(id = row[0], name = row[1].decode("latin-1"), release_date = datetime.strptime(row[2].strip(),"%d-%b-%Y"), imdb_url = row[3])
             # movie = Movie()
             # movie.id =row[0]
             # movie.name =row[1]
@@ -34,13 +34,35 @@ def load_movies(session):
 
 def load_ratings(session):
     # use u.data
-    pass
+    
+    with open('seed_data/u.data', 'rb') as f:
+        # reader = csv.reader(f)
+        # for row in reader:
+        #     print row
+        #     row2= []
+        #     i = 0
+        #     row = row.split(" ")
+        #     for i in row:
+        #         if row[i] != "":
+        #             row2.append(row[i])
+        #         i+=1
+        #     rating = Rating(id = row2[0], movie_id =row2[1], user_id = row2[2], rating = row2[3])
+        #     print rating 
+        for row in f:
+            row = row.split()
+            rating = Rating(id = row[0], movie_id = row[1], user_id = row[2], rating = row[3])
+            print rating
+            session.add(rating)
+        session.commit()
+
+
+
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
     load_users(session)
     load_movies(session)
-    pass
+    load_ratings(session) 
 
 if __name__ == "__main__":
     s= connect()
