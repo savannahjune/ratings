117a907ea4ac2d40794e3ab48d8ec54cbdc01a92
Hackbright Student <info@hackbrightacademy.com>
2014-10-23 18:06:50 -0700

    we fixed dates but text is not in utf8 yet

--- model.py
+++ model.py
@@ -51,3 +51,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+    engine = create_engine("sqlite:///ratings.db", echo=True)
+    Base.metadata.create_all(engine)
--- seed.py
+++ seed.py
@@ -1,4 +1,5 @@
-from model import User, Movie, Rating, connect, Session 
+from model import User, Movie, Rating, connect 
+from datetime import datetime
 import csv
 
 def load_users(session):
@@ -8,14 +9,28 @@ def load_users(session):
         reader = csv.reader(f, delimiter="|")
         for row in reader:
             user = User(id =row[0], age=row[1], zipcode=row[4])
-            print user
+            # print user
             session.add(user)
-            session.commit()
+        session.commit()
 
 
 def load_movies(session):
     # use u.item
-    pass
+    with open('seed_data/u.item', 'rb') as f:
+        reader = csv.reader(f, delimiter="|")
+        for row in reader:
+            if row[2] == "":
+                row[2] = "01-Jan-1970"
+            movie = Movie(id = row[0], name = row[1].encode('utf-8'), release_date = datetime.strptime(row[2].strip(),"%d-%b-%Y"), imdb_url = row[3])
+            # movie = Movie()
+            # movie.id =row[0]
+            # movie.name =row[1]
+            # movie.release_date = datetime.strptime(row[2].strip(),"%d-%b-%Y")
+            # movie.imdb_url=row[3]
+            print movie
+            session.add(movie)
+        session.commit()
+            
 
 def load_ratings(session):
     # use u.data
@@ -24,6 +39,7 @@ def load_ratings(session):
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
     load_users(session)
+    load_movies(session)
     pass
 
 if __name__ == "__main__":
